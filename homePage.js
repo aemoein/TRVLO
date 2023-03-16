c42e0e5a659f7fb589fcb26bae96d3e56211cd3e
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-03-16 15:04:21 +0200

    V2.1
    a slider with images was added to the new homepage to act as destinations

---
+++ homePage.js
@@ -0,0 +1,27 @@
+window.onload=function(){
+    const leftArrow = document.getElementById('left');
+    const rightArrow = document.getElementById('right');
+    const imageContainer = document.getElementById('scroll');
+    
+    let scrollAmount = 0;
+    
+    leftArrow.addEventListener("click", () => 
+    {
+      scrollAmount -= 1200;
+      imageContainer.scrollTo({
+        top: 0,
+        left: scrollAmount,
+        behavior: "smooth"
+      });
+    });
+    
+    rightArrow.addEventListener("click", () => {
+      scrollAmount += 1200;
+      imageContainer.scrollTo({
+        top: 0,
+        left: scrollAmount,
+        behavior: "smooth"
+      });
+    });
+    }
+    
