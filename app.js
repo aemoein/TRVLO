07721a41c8931f0c0991745eb52a5c247256bd14
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-03-16 02:32:33 +0200

    V 1.2
    updates to the home image making the website more friendly to the users and some updates to the header menu and the display

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 // Add an event listener to the "Explore Destinations" button
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+
+menuIcon.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+
 document.querySelector('.hero .btn').addEventListener('click', function(event) {
     // Prevent the default behavior of following the link
     event.preventDefault();
