827520027a80915e22cb00aaf521a95585299da9
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-04-08 17:13:45 +0200

    V.12
    Major update to the data structure and architecture of the system. Additional updates to the destinations page

--- destination.js
+++ destination.js
@@ -35,5 +35,90 @@ window.onload=function(){
         card.style.display = 'none';
         }
     });
-    })
-  }  
+  })
+
+  /*fetch('destination.json')
+  .then(response => response.json())
+  .then(data => {
+    const cities = data.card;
+    const container = document.getElementById('cards');
+    cities.forEach(city => {
+      const card = document.createElement('section'); // create section element
+      card.classList.add('card');
+      card.innerHTML = `
+        <div class="card__img" style="background-image: url('${city.image}')"></div>
+        <a href="${city.link}" class="card_link">
+          <div class="card__img--hover" style="background-image: url('${city.image}')"></div>
+        </a>
+        <div class="card__info">
+          <span class="card__category">${city.category}</span>
+          <h3 class="card__title">${city.name}</h3>
+        </div>
+      `;
+      container.appendChild(card);
+    });
+  });*/
+
+  // Load the JSON data
+  fetch('destination.json')
+  .then(response => response.json())
+  .then(data => {
+    // Create an array of cities
+    const cities = data.card;
+
+    let currentIndex = 0;
+
+    // Display the first 10 cities
+    displayCities(currentIndex);
+
+    // Implement the functionality for the arrows
+    const prevButton = document.getElementById('prev');
+    prevButton.addEventListener('click', () => {
+      displayCities(currentIndex - 9);
+    });
+
+    const nextButton = document.getElementById('next');
+    nextButton.addEventListener('click', () => {
+      displayCities(currentIndex + 9);
+    });
+
+    function displayCities(startIndex) {
+      // Clear the previous list of cities
+      const cityList = document.getElementById('cards');
+      cityList.innerHTML = '';
+
+      // Display the next 10 cities
+      for (let i = startIndex; i < startIndex + 9 && i < cities.length; i++) {
+        const city = cities[i];
+        const listItem = document.createElement('section');
+        listItem.classList.add('card');
+        listItem.innerHTML = `
+        <div class="card__img" style="background-image: url('${city.image}')"></div>
+        <a href="${city.link}" class="card_link">
+          <div class="card__img--hover" style="background-image: url('${city.image}')"></div>
+        </a>
+        <div class="card__info">
+          <span class="card__category">${city.category}</span>
+          <h3 class="card__title">${city.name}</h3>
+        </div>
+      `;
+        //listItem.textContent = city.name;
+        cityList.appendChild(listItem);
+      }
+
+      // Update the current index
+      currentIndex = startIndex;
+
+    let endIndex = currentIndex+8;
+    if (endIndex >= cities.length) {
+        endIndex = cities.length;
+        document.getElementById('next').disabled = true; // Disable the next button
+      } else {
+        document.getElementById('next').disabled = false; // Enable the next button
+    }
+
+    document.getElementById('prev').disabled = currentIndex === 0 ? true : false;
+
+    }
+  });
+}  
--- homePage.js
+++ homePage.js
@@ -23,5 +23,6 @@ window.onload=function(){
         behavior: "smooth"
       });
     });
+
     }
     
