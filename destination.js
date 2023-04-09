f973e71e51377f27d4b619aca014c6deeb73b093
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-04-09 02:41:21 +0200

    V.12.1
    Updates to the destinations interface and more

--- destination.js
+++ destination.js
@@ -1,26 +1,6 @@
 window.onload=function(){
- /* const searchInput = document.getElementById("search-bar");
-  const destinations = document.querySelectorAll(".destination");
-  
-  function filterDestinations() {
-    const searchTerm = searchInput.value.toLowerCase();
-  
-    destinations.forEach((destination) => {
-      const title = destination.querySelector("h3").textContent.toLowerCase();
-      const description = destination.querySelector("p").textContent.toLowerCase();
-  
-      if (title.includes(searchTerm)) {
-        destination.style.display = "block";
-      } else {
-        destination.style.display = "none";
-      }
-    });
-  }
-  
-  searchInput.addEventListener("keyup", filterDestinations);*/
-
-  const searchInput = document.querySelector('.search-container input');
-    const cards = document.querySelectorAll('.card');
+  /*const searchInput = document.querySelector('.search-container input');
+  const cards = document.querySelectorAll('.card');
 
     searchInput.addEventListener('keyup', function(event) {
     const query = event.target.value.toLowerCase();
@@ -35,7 +15,89 @@ window.onload=function(){
         card.style.display = 'none';
         }
     });
-  })
+  })*/
+
+  fetch('destination.json')
+  .then(response => response.json())
+  .then(data => {
+    // Create an array of all cities
+    const allCities = data.card;
+
+    // Get the search form and input field
+    const searchForm = document.getElementById('search-form');
+    const searchInput = document.getElementById('search-input');
+
+    // Implement the search functionality
+    searchForm.addEventListener('submit', (event) => {
+      event.preventDefault(); // Prevent the default form submission
+
+      // Get the user's search query
+      const query = searchInput.value.toLowerCase().trim();
+
+      // Filter the cities based on the search query
+      const filteredCities = allCities.filter(city =>
+        city.name.toLowerCase().includes(query) || city.category.toLowerCase().includes(query)
+      );
+
+      // Display the filtered cities
+      displayCities(0, filteredCities);
+    });
+
+    // Display the first 9 cities on page load
+    displayCities(0, allCities);
+
+    // Implement the functionality for the arrows
+    const prevButton = document.getElementById('prev');
+    prevButton.addEventListener('click', () => {
+      displayCities(currentIndex - 9, allCities);
+    });
+
+    const nextButton = document.getElementById('next');
+    nextButton.addEventListener('click', () => {
+      displayCities(currentIndex + 9, allCities);
+    });
+
+    let currentIndex = 0;
+
+    function displayCities(startIndex, cities) {
+      // Clear the previous list of cities
+      const cityList = document.getElementById('cards');
+      cityList.innerHTML = '';
+
+      const citiesList = cities;
+
+      // Display the next 9 cities
+      for (let i = startIndex; i < startIndex + 9 && i < citiesList.length; i++) {
+        const city = citiesList[i];
+        const listItem = document.createElement('section');
+        listItem.classList.add('card');
+        listItem.innerHTML = `
+        <div class="card__img" style="background-image: url('${city.image}')"></div>
+        <a href="${city.link}" class="card_link">
+          <div class="card__img--hover" style="background-image: url('${city.image}')"></div>
+        </a>
+        <div class="card__info">
+          <span class="card__category">${city.category}</span>
+          <h3 class="card__title">${city.name}</h3>
+        </div>
+      `;
+        cityList.appendChild(listItem);
+      }
+
+      // Update the current index
+      currentIndex = startIndex;
+
+      let endIndex = currentIndex + 8;
+      if (endIndex >= cities.length) {
+        endIndex = cities.length;
+        document.getElementById('next').disabled = true; // Disable the next button
+      } else {
+        document.getElementById('next').disabled = false; // Enable the next button
+      }
+
+      document.getElementById('prev').disabled = currentIndex === 0 ? true : false;
+    }
+  });
 
   /*fetch('destination.json')
   .then(response => response.json())
@@ -109,16 +171,15 @@ window.onload=function(){
       // Update the current index
       currentIndex = startIndex;
 
-    let endIndex = currentIndex+8;
-    if (endIndex >= cities.length) {
-        endIndex = cities.length;
-        document.getElementById('next').disabled = true; // Disable the next button
-      } else {
-        document.getElementById('next').disabled = false; // Enable the next button
-    }
+      let endIndex = currentIndex+8;
+      if (endIndex >= cities.length) {
+          endIndex = cities.length;
+          document.getElementById('next').disabled = true; // Disable the next button
+        } else {
+          document.getElementById('next').disabled = false; // Enable the next button
+      }
 
-    document.getElementById('prev').disabled = currentIndex === 0 ? true : false;
-
+      document.getElementById('prev').disabled = currentIndex === 0 ? true : false;
     }
   });
 }  
